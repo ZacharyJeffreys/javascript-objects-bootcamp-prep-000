6bf804cee987c5311346ac9f4570db1e45959aa6
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-25 02:11:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist = {
-  artistName: songTitle
+  artistName: 'something',
+  artist2: 'something2',
+  artist3: 'something3'
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
